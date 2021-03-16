279cb68bb8821faead7c3dd07f47c9e4c08138b8
EmilNadimanov <nadimaemi@gmail.com>
2021-03-16 20:32:16 +0300

    reordered methods, added formatting for strings, added docstring

--- chukchi/CharTree/CharTree.py
+++ chukchi/CharTree/CharTree.py
@@ -8,8 +8,26 @@ HOW TO:
 4. OPTIONAL: save model by simply printing the tree. You can then simply pass this chunk of text to python interpreter
    and it will create the CharTree object you need, implying you have imported CharTree and TreeLeaf classes, of course.
 """
-from nltk.tokenize import word_tokenize
+import string
 from typing import Dict, Any, Optional
+import re
+
+from nltk.tokenize import word_tokenize, sent_tokenize
+
+
+def _format_input_data(data):
+    _data = data
+    _data = re.sub(r'ль', 'ԓь', _data)
+    _data = re.sub(r'Ль', 'Ԓь', _data)
+    _data = re.sub(r"['`ʼ’\"“]", 'ʼ', _data)
+    _data = re.sub(r"кʼ", 'ӄ', _data)
+    _data = re.sub(r"Kʼ", 'Ӄ', _data)
+    _data = re.sub(r"нʼ", 'ӈ', _data)
+    _data = re.sub(r"Нʼ", 'Ӈ', _data)
+    # replace latin with cyrillic AND drop punctuation
+    _data = _data.translate(str.maketrans('eyopac', 'еуорас', string.punctuation))
+    _data = re.sub(r"\d", '', _data)
+    return _data
 
 
 class CharTree:
@@ -39,6 +57,11 @@ class CharTree:
     def __hash__(self):
         return self.data.__hash__()
 
+    def __repr__(self):
+        root = f"CharTree(data='{self.data}', children={{"
+        children = self.__repr_children()
+        return root + children + "})"
+
     def __build_branch(self, word: str):
         self.children[word[0]] = self.children.get(word[0], CharTree(data=word[0]))
         child: CharTree = self.children[word[0]]
@@ -52,7 +75,9 @@ class CharTree:
     @staticmethod
     def build_tree(corpus: str):
         tree = CharTree(data="")
-        for word in word_tokenize(corpus):
+        _corpus = corpus
+        _corpus = _format_input_data(_corpus)
+        for word in word_tokenize(_corpus):
             tree.__build_branch(word.lower())
             # windows = self.__sliding_window__(word)
             # for w in windows:
@@ -74,17 +99,6 @@ class CharTree:
             accumulate += ","
         return accumulate[:-1]  # dropping the last comma - it's redundant and even harmful
 
-    def __repr__(self):
-        root = f"CharTree(data='{self.data}', children={{"
-        children = self.__repr_children()
-        return root + children + "})"
-
-    def printout(self, level=0):
-        for key in self.children.keys():
-            child = self.children[key]
-            print('\t' * level, f"{key}-{child.count}")
-            child.printout(level + 1)
-
     def __get_matching_subtree(self, typed_text) -> Optional['CharTree']:
         try:
             child = self.children[typed_text[0]]
@@ -103,8 +117,17 @@ class CharTree:
         else:
             return most_probable_letter + child.__get_most_probable_continuation()
 
+    def printout(self, level=0):
+        """
+        A simple visualisation of the CharTree object
+        """
+        for key in self.children.keys():
+            child = self.children[key]
+            print(level, '|', '\t' * level, f"{key}-{child.count}", sep='')
+            child.printout(level + 1)
+
     def predict(self, typed_text: str):
-        __lowered_text = typed_text.lower()
+        __lowered_text = _format_input_data(typed_text.lower())
         match = self.__get_matching_subtree(__lowered_text)
         if match is None:
             return None
